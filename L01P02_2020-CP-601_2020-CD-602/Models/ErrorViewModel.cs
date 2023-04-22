98579e99d3d5a650d0a7df9fb14d0c6e21c9c3a2
jonathan <alcidescp@hotmail.com>
2023-04-22 10:15:08 -0600

    Agregar archivos de proyecto.

---
+++ L01P02_2020-CP-601_2020-CD-602/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P02_2020_CP_601_2020_CD_602.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
